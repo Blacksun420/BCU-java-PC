90837558ca02461d3bd10549df3b1e304fccde29
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2022-11-17 19:34:46 -0400

    Fix JTA dual enter bug
    fffffff

--- src/main/java/page/JTA.java
+++ src/main/java/page/JTA.java
@@ -3,8 +3,6 @@ package page;
 import javax.swing.*;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.function.Consumer;
 
 public class JTA extends JTextArea implements CustomComp {
@@ -17,16 +15,6 @@ public class JTA extends JTextArea implements CustomComp {
 
     public JTA(String tos) {
         super(tos);
-
-        addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent ke) {
-                if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
-                    append("\n");
-                }
-            }
-        });
-
     }
 
     public void setLnr(Consumer<FocusEvent> c) {
