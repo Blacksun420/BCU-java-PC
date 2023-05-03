041eac2d9cca47fc75fdd2ab8cfdb853e446b0e5
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2023-05-03 10:40:36 -0400

    0-6-0-6

--- src/main/java/main/MainBCU.java
+++ src/main/java/main/MainBCU.java
@@ -301,7 +301,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 60005;
+	public static final int ver = 60006;
 	private static final DecimalFormat df = new DecimalFormat("#.##");
 	public static int autoSaveTime = 0;
 	public static final boolean WRITE = !new File("./.idea").exists();
