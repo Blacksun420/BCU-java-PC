c646851fe4190c347fbf3bc6c3ae6b800dfe6e93
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2023-06-04 23:07:42 -0400

    0-6-1-0
    Yeah this is more fitting lmao

--- src/main/java/main/MainBCU.java
+++ src/main/java/main/MainBCU.java
@@ -298,7 +298,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 60010;
+	public static final int ver = 60100;
 	private static final DecimalFormat df = new DecimalFormat("#.##");
 	public static int autoSaveTime = 0;
 	public static final boolean WRITE = !new File("./.idea").exists();
